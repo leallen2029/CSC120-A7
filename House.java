8a738663effa60da07ea10c8f349820928d29a65
Landree Allen <leallen@smith.edu>
2026-03-30 18:51:32 -0400

    lib

--- House.java
+++ House.java
@@ -5,13 +5,11 @@ public class House extends Building implements HouseRequirements {
   private ArrayList<Student> residents;
   private boolean hasDiningRoom;
   public int nResidents;
-  ;
 
   public House(String name, String address, int nFloors, boolean hasDiningRoom) {
         super(name, address, nFloors);
         this.residents = new ArrayList<Student>();
         this.hasDiningRoom = hasDiningRoom;
-        System.out.println("You have built a house: 🏠");
     }
 
   public boolean hasDiningRoom() {
@@ -26,8 +24,9 @@ public class House extends Building implements HouseRequirements {
     this.residents.add(student);
   }
 
-  public void moveOut(Student student) {
+  public Student moveOut(Student student) {
     this.residents.remove(student);
+    return student;
   }
 
   public boolean isResident(Student student) {
@@ -36,5 +35,6 @@ public class House extends Building implements HouseRequirements {
 
     public static void main(String[] args) {
       new House("Landree's house", "100 Green Street Northampton, MA 01063", 2, true);
+      System.out.println("You have built a house: 🏠");
     }
   }
--- Library.java
+++ Library.java
@@ -1,12 +1,45 @@
 /* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
+
+public class Library extends Building implements LibraryRequirements {
+  public boolean isAvailable(String title);
+  private Hashtable<String, Boolean> collection;
 
     public Library() {
+      collection = new Hashtable<>();
       System.out.println("You have built a library: 📖");
     }
-  
+
+    public void addBook(String title, boolean isAvailable) {
+      collection.put(title, isAvailable);
+    }
+    public String removeTitle(String title){
+      returnBook(title);
+      return title;
+    }
+
+    public void checkOut(String title) {
+      collection.put(title, false);
+    }
+
+    public void returnBook(String title) {
+      collection.put(title, true);
+    }
+    public boolean containsTitle(String title){
+      return collection.containsKey(title);
+    }
+    // returns true if the title appears as a key in the Libary's collection, false otherwise
+    public boolean isAvailable(String title){
+      return collection.get(title);
+    } // returns true if the title is currently available, false otherwise
+    public void printCollection(){ // prints out the entire collection in an easy-to-read way (including checkout status)
+      for (String title : collection.keySet()) {
+        System.out.println(title + ": " + (collection.get(title) ? "Available" : "Checked Out"));
+      }
+}
     public static void main(String[] args) {
       new Library();
     }
-  
+
+
   }
