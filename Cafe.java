e62a90e655e1f87128f47b80c1a6ded0b6417220
Landree Allen <leallen@smith.edu>
2026-03-31 19:51:18 -0400

    working

--- Cafe.java
+++ Cafe.java
@@ -14,10 +14,30 @@ public class Cafe extends Building implements CafeRequirements {
         System.out.println("You have built a cafe: ☕");
     }
 
-    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
-        
-        private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
 
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) { 
+        // if you dont have enough materials to make the coffee, restock the materials and then make the coffee
+        if (nCoffeeOunces > this.nCoffeeOunces) {
+            this.nCoffeeOunces += nCoffeeOunces;
+        }
+        if (nSugarPackets > this.nSugarPackets) {
+            this.nSugarPackets += nSugarPackets;
+        }
+        if (nCreams > this.nCreams) {
+            this.nCreams += nCreams;
+        }
+        if (nCups > this.nCups) {
+            this.nCups += nCups;
+        }
+    }
+
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+        this.sellCoffee(100, 100, 100);
+        if (size > this.nCoffeeOunces || nSugarPackets > this.nSugarPackets || nCreams > this.nCreams || 1 > this.nCups) {
+            System.out.println("Here is your coffee! ☕");
+        } else {
+            restock(size, nSugarPackets, nCreams, nCreams);
         }
     }
 
